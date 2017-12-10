2d7afd0cb66318af10c7cfe621a5eef59ac4832e
Elinor Roosalu <elinorroosalu@users.noreply.github.com>
2017-12-10 12:48:05 +0200

    kasutaja enda kuulutuste lehe uuendus

--- userAds.php
+++ userAds.php
@@ -1,21 +1,47 @@
 <?php
-	require("functions.php");
+	require("function.php");
 
 	$notice = "";
 	$ads = "";
 
 	//kui pole sisse logitud, liigume login lehele
 	if(!isset($_SESSION["userId"])){
-		header("Location: login.php");
+		header("Location: main.php");
 		exit();
 	}
 	
 	//väljalogimine
 	if(isset($_GET["logout"])){
 		session_destroy(); //lõpetab sessiooni
-		header("Location: login.php");
+		header("Location: main.php");
 	}
+
 	
-	
+	$ads=readUserAds()
 
 ?>
+<DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style/general.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+</head>
+<body class = "bg-info">
+	<div class="container-fluid text-white">
+	<div class="row">
+	<div class="col-sm-2">
+	<br>
+	<h3>Tere<?php echo " " .$_SESSION["userName"]; ?></h3>
+	<button><a href="?logout=1">Logi välja</a></button><br><br>
+	<button><a href="main.php">Pealeht</a></button><br><br>
+	</div>
+	<div class="col-sm-8">
+	<h1>EGJ VEEBIPOOD</h1>
+	<h2>Kuulutused</h2>
+		<?php echo $ads; ?>
+	</div>
+	</div>
+	</div>
+</body>
+</html>
